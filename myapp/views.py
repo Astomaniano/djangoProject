8146e09308753775a871dacabd3d9876f945b234
Astomaniano <alonso.astomaniano@gmail.com>
2024-09-28 11:08:01 +0400

    homework done

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def data_view(request):
+    return HttpResponse("Страница Data")
+
+def test_view(request):
+    return HttpResponse("Страница Test")
